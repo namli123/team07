7b5b8252a4f09251a40062010af4ed0ca9db64ea
howpan <phannhuthao2001@gmail.com>
2022-10-13 10:31:05 +0800

    first commit

---
+++ database/migrations/2022_10_13_014046_create_catelogs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCatelogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catelogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->comment('名稱')->nullable(false);
+            $table->tinyInteger('mid')->unsigned()->comment('廠商編號')->nullable(false);
+            $table->double('price')->comment('價格')->nullable(false);
+            $table->double('evaluaation')->comment('評價')->nullable(false);
+            $table->date('issue_date')->comment('發行日期')->nullable(true);
+            $table->double('revenue')->comment('硬手')->nullable(false);
+            $table->string('game_type',50)->comment('遊戲類型')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catelogs');
+    }
+}
---
+++ database/migrations/2022_10_13_014620_create_manufacturers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateManufacturersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('manufacturers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->comment('遊戲廠商公司')->nullable(false);
+            $table->tinyInteger('capital')->comment('資本額')->nullable(false);
+            $table->date('found_at')->comment('成立日期')->nullable(true);
+            $table->string('national',100)->comment('國籍')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('manufacturers');
+    }
+}
